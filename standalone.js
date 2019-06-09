e86b0d56e01dfd20f944e6d3bf8fc36dee519db3
Tiago Santos <tandrade@scottishpower.com>
2019-06-09 12:05:35 +0100

    renamed config location to config name

--- standalone.js
+++ standalone.js
@@ -1,6 +1,6 @@
 const fakerlogs = require('./lib');
 const { getConfiguration } = require('./lib/config')
 
-const logConfiguration = getConfiguration(process.env.FAKER_LOGS_CONFIG_LOCATION)
+const logConfiguration = getConfiguration(process.env.FAKER_LOGS_CONFIG_NAME)
 
 fakerlogs(logConfiguration);
